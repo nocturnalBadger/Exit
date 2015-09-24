5b854c201d15b0c5f7d1eb8729a0fc50975350cf
Jeremy Schmidt <jschmidt@notMyEmail.net>
2015-09-24 15:58:43 -0400

    Added files

---
+++ Exit/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Exit
+//
+//  Created by Jeremy Schmidt on 6/16/15.
+//  Copyright © 2015 Jeremy Schmidt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func exitButtonWasPressed(sender: AnyObject)
+    {
+        exit(0)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
